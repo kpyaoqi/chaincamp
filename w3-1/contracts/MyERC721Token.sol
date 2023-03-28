518b74f27c5ca941888b25ebab2f8d6429a1c633
kpyaoqi <yaoqi2136712851@gmail.com>
2023-03-28 11:47:21 +0800

    w3

---
+++ w3-1/contracts/MyERC721Token.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyERC721Token is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("yaoqi", "KPS") {}
+
+    //  QmZNFPwox146ohY93ViFD8omSThRAVYF1A96MNHbWoa2Nr
+    function mint(address student, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(student, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
